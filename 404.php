a501fa37d52cb00c13e5f9a2c2c9fa297ac10bf9
Sydney Edwards <sydneyedwards@sydneys-air-2.attlocal.net>
2019-03-24 22:51:16 -0400

    add question mark

--- 404.php
+++ 404.php
@@ -7,7 +7,7 @@ get_header(); ?>
     <div class="twelve columns not-found">
       <h2>Page not found</h2>
       <h3>This usually never happens!</h3>
-      <p> It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
+      <p> It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home?</a></p>
     </div>
   </div>
 </div>

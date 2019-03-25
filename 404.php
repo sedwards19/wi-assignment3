99bbc2690efa209afa08e1309a55be31192becfd
Sydney Edwards <sydneyedwards@sydneys-air-2.attlocal.net>
2019-03-24 22:49:53 -0400

    correct spelling to 404

--- 404.php
+++ 404.php
@@ -6,7 +6,7 @@ get_header(); ?>
   <div class="row">
     <div class="twelve columns not-found">
       <h2>Page not found</h2>
-      <h3>This usually never happends</h3>
+      <h3>This usually never happens!</h3>
       <p> It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
     </div>
   </div>

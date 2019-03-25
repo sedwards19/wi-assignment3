e32a47447adc2ba79a3368bb1cf07aa3cba5f2e3
Sydney Edwards <sydneyedwards@sydneys-air-2.attlocal.net>
2019-03-24 22:47:30 -0400

    add class

--- 404.php
+++ 404.php
@@ -4,7 +4,7 @@ get_header(); ?>
 
 <div class="container">
   <div class="row">
-    <div class="twelve columns">
+    <div class="twelve columns not-found">
       <h2>Page not found</h2>
       <h3>This usually never happends</h3>
       <p> It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>

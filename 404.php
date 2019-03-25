0046b2fa47f718ee66a81e0ce4f3bc5622c4b253
Sydney Edwards <sydneyedwards@sydneys-air-2.attlocal.net>
2019-03-24 22:45:18 -0400

    add 404 styling

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php /*Template Name: 404 Page */
+
+get_header(); ?>
+
+<div class="row">
+  <div class="twelve columns">
+    <h2>Page not found</h2>
+    <h3>This usually never happends</h3>
+    <p> It looks like nothing was found at this location. Would you like to go back <a href="<?php echo home_url('/');?>">home</a></p>
+  </div>
+</div>
+
+<?php get_footer(); ?>
